1d8b1953999cedf3eec958c67dc5b7556deb41a3
Skillhh <ortegon.giovanny@hotmail.com>
2019-09-05 10:47:16 -0700

    [ADD] 8086 files

---
+++ 8086/pgm.asm
@@ -0,0 +1,19 @@
+.model small
+data segment
+a db 09h
+b db 02h
+c dw ?
+data ends
+ 
+code segment
+assume cs:code,ds:data
+start:
+mov ax,data
+mov ds,ax
+mov al,a
+mov bl,b
+add al,bl
+mov c,ax
+int 3
+code ends
+end start
