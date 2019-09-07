d91cc9f3c577b0200515fad69f24e01e242cd377
Skillhh <ortegon.giovanny@hotmail.com>
2019-09-07 10:28:10 -0700

    [ADD] examples of INC DEC NEG

---
+++ masm/ex_inc_dec_neg.asm
@@ -0,0 +1,28 @@
+.model small
+
+.data
+
+num1    db  6
+num2    db  3
+def     db  0
+
+.code
+
+main proc
+    
+    mov ax, @data
+    mov ds, ax
+    
+    inc num1
+    inc num1
+    
+    dec num2
+    dec num2
+    
+    mov al, num2
+    neg al
+    add al, num1
+    mov def, al   
+    
+    main endp
+end
