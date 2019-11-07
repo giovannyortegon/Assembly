e41d0bac3c2d7f5cf979fe30fc0a2174fd36ef20
Skillhh <ortegon.giovanny@hotmail.com>
2019-11-07 12:57:58 -0500

    [ADD] files eamples

---
+++ ATT/for.s
@@ -0,0 +1,71 @@
+	.file	"for.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"The answer is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ebx
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x78,0x6
+	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
+	subl	$16, %esp
+	call	__x86.get_pc_thunk.bx
+	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
+	movl	$0, -12(%ebp)
+	movl	$0, -12(%ebp)
+	jmp	.L2
+.L3:
+	movl	-12(%ebp), %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	movl	%eax, -16(%ebp)
+	subl	$12, %esp
+	leal	.LC0@GOTOFF(%ebx), %eax
+	pushl	%eax
+	call	printf@PLT
+	addl	$16, %esp
+	addl	$1, -12(%ebp)
+.L2:
+	cmpl	$99, -12(%ebp)
+	jle	.L3
+	movl	$0, %eax
+	leal	-8(%ebp), %esp
+	popl	%ecx
+	.cfi_restore 1
+	.cfi_def_cfa 1, 0
+	popl	%ebx
+	.cfi_restore 3
+	popl	%ebp
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.bx,"axG",@progbits,__x86.get_pc_thunk.bx,comdat
+	.globl	__x86.get_pc_thunk.bx
+	.hidden	__x86.get_pc_thunk.bx
+	.type	__x86.get_pc_thunk.bx, @function
+__x86.get_pc_thunk.bx:
+.LFB1:
+	.cfi_startproc
+	movl	(%esp), %ebx
+	ret
+	.cfi_endproc
+.LFE1:
+	.ident	"GCC: (Debian 9.2.1-16) 9.2.1 20191030"
+	.section	.note.GNU-stack,"",@progbits
