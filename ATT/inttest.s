723317587ee896845451fa46489ebfc16330106f
Skillhh <ortegon.giovanny@hotmail.com>
2019-12-05 01:00:02 -0500

    [ADD] inttest signed integer

---
+++ ATT/inttest.s
@@ -0,0 +1,15 @@
+# inttest.s - An example of using signed integers
+.section .data
+data:
+	.int -45
+.section .text
+	.globl _start
+
+_start:
+	nop
+	movl $-345, %ecx
+	movw $0xffb1, %dx
+	movl data, %ebx
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
