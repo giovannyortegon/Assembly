bbc0c32f50b43758603a8f9b88d175861f4ee6f4
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2020-01-05 00:06:39 -0500

    [ADD] File SUB MUL, DIV

---
+++ ATT/Chp8/subtest3.s
@@ -0,0 +1,25 @@
+# subtest3.s - An example of an overflow condition in a SUB instruction
+.section .data
+output:
+	.asciz "The result is %d\n"
+.section .text
+.globl _start
+_start:
+	nop
+	movl $-1590876934, %ebx
+	movl $-1259230143, %eax
+	subl %eax, %ebx
+	jo over
+	pushl %ebx
+	pushl $output
+	call printf
+	add $8, %esp
+	pushl $0
+	call exit
+over:
+	pushl $0
+	pushl $output
+	call printf
+	add $8, %esp
+	pushl $0
+	call exit
