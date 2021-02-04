3ea08bd72389c9197d809f61d10eb4b68e2a7ee9
Skillhh <ortegon.giovanny@hotmail.com>
2021-02-04 16:14:23 -0500

    [UPDATE] add result fibonacci to array in section data and bss

--- ATT/Exercise/x86/fibonacci.s
+++ ATT/Exercise/x86/fibonacci.s
@@ -1,7 +1,9 @@
 # fibonacci.s
 .section .data
 	msg: .asciz "%d "
-	
+	buf: .int 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+.section .bss
+	.lcomm buffer, 20
 .section .text
 	.globl _start
 _start:
@@ -10,21 +12,29 @@ _start:
 	movl $0, %ebx
 	movl $0, %ecx
 	movl $0, %edx
+	movl $0, %edi
 	
 	pushl %eax
 	pushl %ebx
 
 	movl %eax, %ebx
 
+	movl %ebx, buffer(, %edi, 4)
+	movl %ebx, buf(, %edi, 4)
+#	pushl %edi
+
 	push %ebx
 	pushl $msg
 	call printf
 	addl $8, %esp
 
+#	pop %edi
 	pop %ebx
 	pop %eax
+
+	inc %edi
 L2:
-	cmpl $20, %edx
+	cmpl $10, %edx
 	jl L1
 	jmp end
 
@@ -32,19 +42,25 @@ L1:
 	pushl %edx
 	addl %eax, %ebx
 	pushl %eax
+	
+	movl %ebx, buffer(, %edi, 4)
+	movl %ebx, buf(, %edi, 4)
+#	pushl %edi
 
 	pushl %ebx
 	pushl $msg
 	call printf
 	addl $8, %esp
-
+	
+#	popl %edi
 	popl %eax
 	movl %ebx, %ecx
 	movl %eax, %ebx
 	movl %ecx, %eax
 
 	popl %edx
-	incl %edx
+	inc %edx
+	inc %edi
 	jmp L2
 
 end:
