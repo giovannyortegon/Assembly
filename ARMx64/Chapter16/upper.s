265636b4e21fa3079683ba43d4068e291257f700
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-07-18 12:34:14 -0500

    [ADD] hacking code arm

--- ARMx64/Chapter16/upper.s
+++ ARMx64/Chapter16/upper.s
@@ -1,29 +1,35 @@
-/*
- * Assembler program to convert a string to
- * all upper case
- *
- * x1 - address of output string
- * x0 - address of input string
- * x4 - original output string for length calc.
- * w5 - current character being processed
- */
-	.global toupper		// Allow other files to call this routine
-toupper:
-	mov x4, x1
-// The loop is until byte pointed to x1 is non-zero
-loop:
-	ldrb w5, [x0], #1 // load character and incr ptr
-	// if w5 > 'z'	then goto cont
-	cmp w5, #'z'		// is letter > 'z'?
-	b.gt cont			// goto to end if
-	cmp w5, #'a'
-	b.lt cont			// goto to end if
-// convert it
-	sub w5, w5, #('a' - 'A')  // 32
-cont:
-	strb w5, [x1], #1	// store character to output str
-	cmp w5, #0			// stop null char
-	b.ne loop			// if char is not null
-	sub x0, x1, x4		// get the len by subing the ptr
+// Assembler program to convert a string to
+// all upper case.
+  .global _start
+_start:
+  ldr x4, =instr      // start of input string
+  ldr x3, =outstr     // output string
 
-	ret					// Return to caller
+// loop is until byte pointed to non-zero
+loop:
+  ldrb w5, [x4], #1     // load character and incr pointer
+// if w5 > 'z'
+  sub w6, w5, #'a'
+  cmp w6, #25
+// else if w5 < 'a' then goto end if
+  sub w6, w5, #('a'-'A')
+  csel w5, w6, w5, ls
+  strb w5, [x3], #1     // store character to output str
+  cmp w5, #0            // stop on hitting a null character
+  b.ne loop             // loop if character is not null
+
+// setup to print
+  mov x0, #1            // 1 = stdout
+  ldr x1, =outstr       // string to print
+  sub x2, x3, x1        // get the length by sub'ing the pointers
+  mov x8, #64           // syscall
+  svc 0
+
+// exit
+  mov x0, #0
+  mov x8, #93           // syscall
+  svc 0
+
+  .data
+instr:  .asciz "This is our Test String that we will convert.\n",
+outstr: .fill 255, 1, 0
