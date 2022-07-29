3a3b0a757dbf8f7c9340d441dadf18253ac8a302
giovanny ortegon <ortegon.giovanny@hotmail.com>
2022-07-29 14:14:03 -0500

    [ADD] 2D matrix

--- x86Core/chap02/CalcMatrixRowColSums.cpp
+++ x86Core/chap02/CalcMatrixRowColSums.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
-#include <tchar.h>
 #include <stdlib.h>
+#include <tchar.h>
 
+void PrintResults(const int * x, int nrows, int ncols, int * row_sums, int * col_sums);
 extern "C" int CalcMatrixRowColSums_(const int * x, int nrows, int ncols, int * row_sums, int * col_sums);
-void  CalcMatrixRowColSumsCpp(const int * x, int nrows, int ncols, int * row_sums, int * col_sums);
-void  PrintResults(const int * x, int nrows, int ncols, int * row_sums, int * col_sums);
+void CalcMatrixRowColSumsCpp(const int * x, int nrows, int ncols, int * row_sums, int * col_sums);
 
-int _tmain(int argc, _TCHAR * argv[]){
+int _tmain(int argc, _TCHAR * argv[])
+{
 	const int nrows = 7, ncols = 5;
 	int x[nrows][ncols];
 
-	// Initialize  the test matrix
+	// Initialize the matrix
 	srand(13);
-	for (int i = 0; i < nrows; i++) {
-		for (int j = 0; j < ncols; j++) {
-			x[i][j] = rand() % 100;
-		}
-	}
 
-	// Calculate the row and column sums
+	for (int i = 0; i < nrows; i++) {
+		for (int j = 0; j < ncols; j++)
+			x[i][j] = rand() % 100;
+	}
+   
+	// calculate the row and columns sums
 	int row_sums1[nrows], col_sums1[ncols];
 	int row_sums2[nrows], col_sums2[ncols];
 
 	CalcMatrixRowColSumsCpp((const int *)x, nrows, ncols, row_sums1, col_sums1);
-
 	std::cout << "Result using CalcMatrixRowColSumsCpp()\n";
 	PrintResults((const int *)x, nrows, ncols, row_sums1, col_sums1);
 
@@ -31,10 +31,11 @@ int _tmain(int argc, _TCHAR * argv[]){
 	std::cout << "\n\nResult using CalcMatrixRowColSums_()\n";
 	PrintResults((const int *)x, nrows, ncols, row_sums2, col_sums2);
 
-	return(0);
+
+	return (0);
 }
 
-void  CalcMatrixRowColSumsCpp(const int * x, int nrows, int ncols, int * row_sums, int * col_sums) {
+void CalcMatrixRowColSumsCpp(const int * x, int nrows, int ncols, int * row_sums, int * col_sums) {
 	for (int j = 0; j < ncols; j++)
 		col_sums[j] = 0;
 
@@ -49,15 +50,19 @@ void  CalcMatrixRowColSumsCpp(const int * x, int nrows, int ncols, int * row_sum
 		}
 	}
 }
+
 void  PrintResults(const int * x, int nrows, int ncols, int * row_sums, int * col_sums) {
+	std::cout << std::endl <<std::endl;
 	for (int i = 0; i < nrows; i++) {
 		int k = i * ncols;
 		for (int j = 0; j < ncols; j++) {
-			std::cout << x[k + j] <<"\t";
+			std::cout << x[k + j] << "\t";
 		}
-		std::cout << " -- " << row_sums[i] << std::endl;
+		std::cout << " -- " << row_sums[i] << std::endl;	
 	}
-	for (int i = 0; i < ncols; i++){
-		std::cout << col_sums[i] <<"\t";
+	std::cout << std::endl;
+
+	for (int i = 0; i < ncols; i++) {
+		std::cout << col_sums[i] << "\t";
 	}
 }
