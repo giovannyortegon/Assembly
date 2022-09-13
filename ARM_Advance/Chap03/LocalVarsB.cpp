8cc9d6aba66ac9263fd9364ca1db95434f7d3c63
giovannyortegon <ortegon.giovanny@hotmail.com>
2022-09-13 16:36:11 -0500

    [ADD] Local Variable using stack framework

--- ARM_Advance/Chap03/LocalVarsB.cpp
+++ ARM_Advance/Chap03/LocalVarsB.cpp
@@ -1,34 +1,42 @@
-#include <iostream>
-#include <iomanip>
-
-extern "C" void LocalVarsA_(int, int, int, int, int, int, int *, int *);
-void LocalVarsA(int, int, int, int, int, int, int *, int *);
-void PrintResult(const char *, int, int, int, int, int, int, int, int, int, int);
+#include<iostream>
+#include<iomanip>
 
 using namespace std;
 
+extern "C" void LocalVarsB_(int a, int b, int c, int d, int e, int f, int * g, int * h);
+void LocalVarsB(int a, int b, int c, int d, int e, int f, int *g, int *h);
+void PrintResult(const char * msg, int a, int b, int c, int d, int e, int f, int g1, int g2, int h1, int h2);
+
 int main() {
-    int a, b, c, d, e, f, g1, g2, h1, h2;
+int a, b, c, d, e, f, g1, g2, h1, h2;
 
     // Test Caso #1
     a = 10; b = -20; c = 30; d = -40; e = -50; f = -60;
-    LocalVarsA(a, b, c, d, e, f, &g1, &h1);
-    LocalVarsA_(a, b, c, d, e, f, &g2, &h2);
+    LocalVarsB(a, b, c, d, e, f, &g1, &h1);
+    LocalVarsB_(a, b, c, d, e, f, &g2, &h2);
     PrintResult("Test Case #1", a, b, c, d, e, f, g1, g2, h1, h2);
 
     // Test Caso #1
     a = 100; b = -200; c = 300; d = -400; e = -500; f = -600;
-    LocalVarsA(a, b, c, d, e, f, &g1, &h1);
-    LocalVarsA_(a, b, c, d, e, f, &g2, &h2);
+    LocalVarsB(a, b, c, d, e, f, &g1, &h1);
+    LocalVarsB_(a, b, c, d, e, f, &g2, &h2);
     PrintResult("Test Case #2", a, b, c, d, e, f, g1, g2, h1, h2);
 
     return (0);
 }
-void PrintResult(const char * msg, int a, int b, int c, int d, int e, int f, int g1, int g2, int h1, int h2) {
+void LocalVarsB(int a, int b, int c, int d, int e, int f, int *g, int *h) {
+    int temp0 = a + b + c;
+    int temp1 = d + e + f;
+    int temp2 = a + c + e;
+    int temp3 = b + d + f;
 
+    * g = temp0 * temp1;
+    * h = temp2 * temp3;
+}
+void PrintResult(const char * msg, int a, int b, int c, int d, int e, int f, int g1, int g2, int h1, int h2) {
     const char nl = '\n';
     const char * sep = " | ";
-    const size_t w = 8;
+    const size_t  w = 8;
 
     cout <<msg <<nl;
     cout <<"a = " <<setw(w) <<a <<sep;
@@ -46,14 +54,4 @@ void PrintResult(const char * msg, int a, int b, int c, int d, int e, int f, int
         cout <<"Compare Error!\n";
     }
     cout <<nl;
-}
-
-void LocalVarsA(int a, int b, int c, int d, int e, int f, int *g, int *h) {
-    int temp0 = a + b + c;
-    int temp1 = d + e + f;
-    int temp2 = a + c + e;
-    int temp3 = b + d + f;
-
-    * g = temp0 * temp1;
-    * h = temp2 * temp3;
 }
