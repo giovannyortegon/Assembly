d43f66520a242c3f45f4f1ec6b1cc6f750fe25c1
Giovanny Ortegon <37087716+giovannyortegon@users.noreply.github.com>
2024-01-14 15:51:57 -0500

    [UPDATE]

--- ARMx32/chap15/program15c.s
+++ ARMx32/chap15/program15c.s
@@ -1,36 +1,31 @@
-/* use post indexed addr to concat strings */
+/**** Use post-indexed addr to concat strings ****/
 	.global _start
 _start:
-	ldr r2, =string1		// load locations
-	ldr r3, =string2		// of both strings
-
+	LDR R2, =string1	@ load locations
+	LDR R3, =string2	@ of both strings
 _loop:
-	ldrb r0, [r3], #1		// Get string2 byte & + 1
-	cmp r0, #0				// is it end of string?
-	bne _loop				// no, then get next byte
-	sub r3, r3, #1			// Yes, decrement back 1
-
+	LDRB R0, [R3], #1	@ Get string2 byte & +1
+	CMP R0, #0			@ is it end of string?
+	BNE _loop			@ no, then get next byte
+	SUB R3, R3, #1 @	@ Yes, decrement back 1
 _copyloop:
-	ldrb r0, [r2], #1		// get byte from string 1
-	strb r0, [r3], #1		// add to end of string 2
-	cmp r0, #0				// is it 0?
-	bne _copyloop			// if not get next char
-
+	LDRB R0, [R2], #1	@ get byte from string 1
+	STRB R0, [R3], #1	@ add to end of string 2
+	CMP R0, #0			@ is it 0?
+	BNE _copyloop		@if not get next char
 _write:
-	mov r0, #1				// is 0 so print new
-	ldr r1, =string2
-	mov r2, #24
-	mov r7, #4
-	swi 0
-
+	MOV R0, #1			@ is 0 so print new
+	LDR R1, =string2
+	MOV R2, #24
+	MOV R7, #4
+	SWI 0
 _exit:
-	mov r7, #1
-	swi 0
-
-	.data
-string1:
-	.asciz "ABCDEFGHIJKL"
-string2:
-	.asciz "012345678910"
-padding:
-	.ascii "            "
+	MOV R7, #1
+	SWI 0
+.data
+	string1
+		.asciz "ABCDEFGHIJKL"
+	string2:
+		.asciz "012345678910"
+	padding:
+		.ascii "            "
