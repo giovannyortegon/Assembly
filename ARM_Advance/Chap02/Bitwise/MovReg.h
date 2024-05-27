0051da7b3cd2849477cea8f3e7386ac9303566f8
giovannyortegon <ortegon.giovanny@hotmail.com>
2024-05-26 19:19:40 -0500

    [RM] Removing Directories

--- ARM_Advance/Chap02/Bitwise/MovReg.h
+++
@@ -1,2 +0,0 @@
-void PrintResultA(const char* msg, unsigned int a, unsigned int b, const unsigned int* c, size_t n);
-void PrintResultB(const char* msg, unsigned int a, const unsigned int* b, size_t n);
