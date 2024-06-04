9b8a54f3dee3ff63156cf17d69fdbd2c5aca0b4c
giovannyortegon <giovanny.ortegon@hotmail.com>
2024-06-03 22:15:49 -0500

    [ADD] arithmetic opration Add and Sub

---
+++ ARM_BlueFox/chap-05/AddSub.s
@@ -0,0 +1,24 @@
+    .global _start
+_start:
+    mov r1, #8
+    mov r2, #4
+    mov r3, #1
+
+    add r4, r1, r2
+    sub r4, r1, r2
+
+    add r1, #10
+    sub r1, #10
+
+    add r4, r1, r2, RRX
+    sub r4, r1, r2, RRX
+
+    add r4, r1, r2, lsl #1
+    sub r4, r1, r2, lsl #1
+
+    add r4, r1, r2, lsl r3
+    sub r4, r1, r2, lsl r3
+
+    eor r0, r0
+    mov r7, #1
+    swi 0
